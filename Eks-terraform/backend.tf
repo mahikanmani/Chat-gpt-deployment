de37d0e3ecf74278e35bbd1c7ca912bc41eb4925
Mahi_Kanmani <148936269+mahikanmani@users.noreply.github.com>
2025-03-19 22:29:30 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "demo1212221" # Replace with your actual S3 bucket name
+    bucket = "chatgpt-19032025" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-2"
   }
 }
